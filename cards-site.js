77eb4fe3a1aca94de8bcee07c89cd468d3cb72d1
mohamed01102579732-sys <mohamed01102579732@gmail.com>
2025-12-26 19:05:21 +0200

    Add files via upload

---
+++ cards-site.js
@@ -0,0 +1,25 @@
+// كل كارت هنا عبارة عن رقم + رسالة + صورة
+const cardsData = {
+
+    1: {
+        text: "I love you Mariomty ❤️",
+        image: "photo1.jpg",
+        audio: "voice1.mp3"
+
+    },
+
+    2: {
+        text: "شكرًا إنك دايمًا جنبي 🙏",
+        image: "images/thanks1.jpg",
+        audio: "media/voice1.mp3"
+
+    },
+
+    3: {
+        text: "إنت شخص مميز جدًا ✨",
+        image: "images/love2.jpg",
+        audio: "media/voice1.mp3"
+
+    }
+
+};
